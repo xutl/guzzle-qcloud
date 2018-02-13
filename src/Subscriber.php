2b7530076cdbec906bb2e35060524bfb4d1aeb4a
xutongle <xutongle@gmail.com>
2018-02-13 11:44:44 +0800

    test

--- src/Subscriber.php
+++ src/Subscriber.php
@@ -5,8 +5,9 @@
  * @license http://www.tintsoft.com/license/
  */
 
-namespace xutl\guzzle\subscriber;
+namespace xutl\guzzle\qcloud;
 
+use function GuzzleHttp\default_user_agent;
 use Psr\Http\Message\RequestInterface;
 
 class Subscriber
@@ -26,7 +27,7 @@ class Subscriber
         $this->config = [
             'secretId' => '123456',
             'secretKey' => '654321',
-            'SignatureMethod' => self::SIGNATURE_METHOD_HMAC_SHA256,
+            'signatureMethod' => self::SIGNATURE_METHOD_HMAC_SHA256,
             'region' => '',
         ];
         foreach ($config as $key => $value) {
@@ -56,15 +57,33 @@ class Subscriber
      */
     private function onBefore(RequestInterface $request)
     {
-        $params = \GuzzleHttp\Psr7\parse_query($request->getBody()->getContents());
-        if (!empty($this->config['region']) && !isset($params['Region'])) {
-            $params['Region'] = $this->config['region'];
+        $request->withAddedHeader('User-Agent', default_user_agent());
+        if ($request->getMethod() == 'GET') {
+            $params = \GuzzleHttp\Psr7\parse_query($request->getUri()->getQuery());
+            $request->withUri($request->getUri()->withQuery($this->makeParams($request, $params)));
+        } else if ($request->getMethod() == 'POST') {
+            $params = \GuzzleHttp\Psr7\parse_query($request->getBody()->getContents());
+            $request->withBody(\GuzzleHttp\Psr7\stream_for($this->makeParams($request, $params)));
         }
+        return $request;
+    }
+
+    /**
+     * 组装参数
+     * @param RequestInterface $request
+     * @param array $params
+     * @return string
+     */
+    public function makeParams(RequestInterface $request, array $params)
+    {
         $params['SecretId'] = $this->config['secretId'];
         $params['Nonce'] = uniqid();
         $params['Timestamp'] = time();
-        $params['RequestClient'] = 'guzzleHttp';
-        $params['SignatureMethod'] = $this->config['SignatureMethod'];
+        $params['RequestClient'] = default_user_agent();
+        $params['SignatureMethod'] = $this->config['signatureMethod'];
+        if (!empty($this->config['region']) && !isset($params['Region'])) {
+            $params['Region'] = $this->config['region'];
+        }
         ksort($params);
         $url = str_replace(['http://', 'https://'], '', $request->getUri());
         $i = 0;
@@ -82,13 +101,11 @@ class Subscriber
         }
         $plainText = $request->getMethod() . $url;
         //签名
-        if ($this->config['SignatureMethod'] == self::SIGNATURE_METHOD_HMAC_SHA256) {
+        if ($this->config['signatureMethod'] == self::SIGNATURE_METHOD_HMAC_SHA256) {
             $params['Signature'] = base64_encode(hash_hmac('sha256', $plainText, $this->config['secretKey'], true));
-        } elseif ($this->config['SignatureMethod'] == self::SIGNATURE_METHOD_HMAC_SHA1) {
+        } elseif ($this->config['signatureMethod'] == self::SIGNATURE_METHOD_HMAC_SHA1) {
             $params['Signature'] = base64_encode(hash_hmac('sha1', $plainText, $this->config['secretKey'], true));
         }
-        $query = \GuzzleHttp\Psr7\build_query($params);
-        $request->withBody($query);
-        return $request;
+        return \GuzzleHttp\Psr7\build_query($params);
     }
 }
